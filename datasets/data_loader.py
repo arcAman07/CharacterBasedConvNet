e34b0c75ad059b5b77b88109b388fd0be63b3391
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-24 14:58:15 +0530

    dataset recipes added

---
+++ datasets/data_loader.py
@@ -0,0 +1,4 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
---
+++ datasets/load.py
@@ -0,0 +1,13 @@
+import tarfile
+import wget
+import pandas as pd
+import numpy as np
+url = "https://s3.amazonaws.com/fast-ai-nlp/dbpedia_csv.tgz"
+ag_news_csv
+amazon_review_full_csv
+amazon_review_polarity_csv
+dbpedia_csv
+yahoo_answers_csv
+sogou_news_csv
+yelp_review_full_csv
+yelp_review_polarity_csv
