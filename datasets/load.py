83f8bc99f55ccc955ed74d68bee8073c65d1d67b
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-24 15:18:10 +0530

    dataset recipes added

--- datasets/load.py
+++ datasets/load.py
@@ -3,7 +3,7 @@ import wget
 import pandas as pd
 import numpy as np
 url = "https://s3.amazonaws.com/fast-ai-nlp/dbpedia_csv.tgz"
-ag_news_csv
+ag_news_csv 
 amazon_review_full_csv
 amazon_review_polarity_csv
 dbpedia_csv
