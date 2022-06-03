2363a19d70c509c4f864193a1b933cbe3e2c7458
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-03 19:50:28 +0530

    Text cleaning functions added

--- utils/text_cleaning.py
+++ utils/text_cleaning.py
@@ -8,3 +8,7 @@ def remove_hashtags(text):
 def remove_user_mentions(text):
     clean_text = re.sub(r"@[A-Za-z0-9_]+", "", text)
     return clean_text
+
+def remove_urls(text):
+    clean_text = re.sub(r"^https?:\/\/.*[\r\n]*", "", text, flags=re.MULTILINE)
+    return clean_text
