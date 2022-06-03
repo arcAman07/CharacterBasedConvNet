f5d40f5014eadecc031f0cd2ce2c0b85f2843f2b
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-03 22:04:22 +0530

    metrics.py added

--- metrics.py
+++ metrics.py
@@ -3,4 +3,14 @@ def plot_accuracies(history):
     plt.plot(accuracies, '-x')
     plt.xlabel('epoch')
     plt.ylabel('accuracy')
-    plt.title('Accuracy vs. No. of epochs');
+    plt.title('Accuracy vs. No. of epochs')
+
+def plot_losses(history):
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs');
