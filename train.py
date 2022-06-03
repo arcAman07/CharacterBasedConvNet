2a0d457528e2534ff6cc6ecbda0d2277fe426a5c
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-03 22:01:23 +0530

    base model added

--- train.py
+++ train.py
@@ -1,1 +1,20 @@
+import os
+import torch
+import torchvision
+import tarfile
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torchvision.datasets.utils import download_url
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.transforms as tt
+from torch.utils.data import random_split
+from torchvision.utils import make_grid
+import matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline
+import pandas as pd
+import seaborn as sns
 
+matplotlib.rcParams['figure.facecolor'] = '#ffffff'
