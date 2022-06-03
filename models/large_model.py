51f21971f1e6b514c18833368feb4ec81b59fd61
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-03 21:24:04 +0530

    Updated loss function

--- models/large_model.py
+++ models/large_model.py
@@ -2,14 +2,5 @@ import json
 import torch
 import torch.nn as nn
 
-class largeCharacterLevelCNN(nn.Module):
-  def __init__(self, args, number_of_classes):
-    super(largeCharacterLevelCNN, self).__init__()
-
-  # define conv layers
-
-  self.dropout_input = nn.Dropout2D(args.dropout_input)
-
-  self.conv1 = nn.Conv2d
 
 
