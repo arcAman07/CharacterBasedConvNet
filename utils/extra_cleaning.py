e06bda64a8ff5eb29d33866c59f79401dc5fd516
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-03 19:31:41 +0530

    Text cleaning functions added

---
+++ utils/extra_cleaning.py
@@ -0,0 +1,19 @@
+def remove_newlines_tabs(text):
+    """
+    This function will remove all the occurrences of newlines, tabs, and combinations like: \\n, \\.
+    
+    arguments:
+        input_text: "text" of type "String". 
+                    
+    return:
+        value: "text" after removal of newlines, tabs, \\n, \\ characters.
+        
+    Example:
+    Input : This is her \\ first day at this place.\n Please,\t Be nice to her.\\n
+    Output : This is her first day at this place. Please, Be nice to her. 
+    
+    """
+    
+    # Replacing all the occurrences of \n,\\n,\t,\\ with a space.
+    Formatted_text = text.replace('\\n', ' ').replace('\n', ' ').replace('\t',' ').replace('\\', ' ').replace('. com', '.com')
+    return Formatted_text
