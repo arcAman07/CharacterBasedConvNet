94308bd3149f499db7556a7c65b4cf0184c7117d
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-03 19:33:46 +0530

    Text cleaning functions added

--- utils/extra_cleaning.py
+++ utils/extra_cleaning.py
@@ -1,3 +1,9 @@
+import bs4
+from bs4 import BeautifulSoup
+import re
+import json
+import math
+
 def remove_newlines_tabs(text):
     """
     This function will remove all the occurrences of newlines, tabs, and combinations like: \\n, \\.
@@ -17,3 +23,67 @@ def remove_newlines_tabs(text):
     # Replacing all the occurrences of \n,\\n,\t,\\ with a space.
     Formatted_text = text.replace('\\n', ' ').replace('\n', ' ').replace('\t',' ').replace('\\', ' ').replace('. com', '.com')
     return Formatted_text
+
+def strip_html_tags(text):
+    """ 
+    This function will remove all the occurrences of html tags from the text.
+    
+    arguments:
+        input_text: "text" of type "String". 
+                    
+    return:
+        value: "text" after removal of html tags.
+        
+    Example:
+    Input : This is a nice place to live. <IMG>
+    Output : This is a nice place to live.  
+    """
+    # Initiating BeautifulSoup object soup.
+    soup = BeautifulSoup(text, "html.parser")
+    # Get all the text other than html tags.
+    stripped_text = soup.get_text(separator=" ")
+    return stripped_text
+  
+def remove_links(text):
+    """
+    This function will remove all the occurrences of links.
+    
+    arguments:
+        input_text: "text" of type "String". 
+                    
+    return:
+        value: "text" after removal of all types of links.
+        
+    Example:
+    Input : To know more about this website: kajalyadav.com  visit: https://kajalyadav.com//Blogs
+    Output : To know more about this website: visit:     
+    
+    """
+    
+    # Removing all the occurrences of links that starts with https
+    remove_https = re.sub(r'http\S+', '', text)
+    # Remove all the occurrences of text that ends with .com
+    remove_com = re.sub(r"\ [A-Za-z]*\.com", " ", remove_https)
+    return remove_com
+
+
+def remove_whitespace(text):
+    """ This function will remove 
+        extra whitespaces from the text
+    arguments:
+        input_text: "text" of type "String". 
+                    
+    return:
+        value: "text" after extra whitespaces removed .
+        
+    Example:
+    Input : How   are   you   doing   ?
+    Output : How are you doing ?     
+        
+    """
+    pattern = re.compile(r'\s+') 
+    Without_whitespace = re.sub(pattern, ' ', text)
+    # There are some instances where there is no space after '?' & ')', 
+    # So I am replacing these with one space so that It will not consider two words as one token.
+    text = Without_whitespace.replace('?', ' ? ').replace(')', ') ')
+    return text
