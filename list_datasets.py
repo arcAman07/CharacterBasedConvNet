6bdb245450257e784e50a49500cf1416db3a3a6f
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-04 22:13:23 +0530

    datasets added

---
+++ list_datasets.py
@@ -0,0 +1,8 @@
+ag_news_csv = "https://s3.amazonaws.com/fast-ai-nlp/ag_news_csv.tgz"
+amazon_review_full_csv = "https://s3.amazonaws.com/fast-ai-nlp/amazon_review_full_csv.tgz"
+amazon_review_polarity_csv = "https://s3.amazonaws.com/fast-ai-nlp/amazon_review_polarity_csv.tgz"
+dbpedia_csv = "https://s3.amazonaws.com/fast-ai-nlp/dbpedia_csv.tgz"
+yahoo_answers_csv = "https://s3.amazonaws.com/fast-ai-nlp/yahoo_answers_csv.tgz"
+sogou_news_csv = "https://s3.amazonaws.com/fast-ai-nlp/sogou_news_csv.tgz"
+yelp_review_full_csv = "https://s3.amazonaws.com/fast-ai-nlp/yelp_review_full_csv.tgz"
+yelp_review_polarity_csv = "https://s3.amazonaws.com/fast-ai-nlp/yelp_review_polarity_csv.tgz"
