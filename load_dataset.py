0d37ae9614672482a4340855c598d77b55d44aad
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 01:19:28 +0000

    Adding tarfile member sanitization to extractall()

--- load_dataset.py
+++ load_dataset.py
@@ -17,7 +17,26 @@ download_url(yelp_review_polarity_csv, '.')
 
 # Extract from archive
 with tarfile.open('./yelp_review_polarity_csv.tgz', 'r:gz') as tar:
-    tar.extractall(path='./data')
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, path="./data")
     
 # Look into the data directory
 data_dir = './data/yelp_review_polarity_csv'
